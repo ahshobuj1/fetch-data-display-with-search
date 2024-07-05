b66691b246fa2097c1500400567b6d5bfeeceefe
A H Shobuj <ahshobuj09@gmail.com>
2024-07-05 21:25:31 +0600

    fetch users data and display the data

--- js/fetch.js
+++ js/fetch.js
@@ -1,27 +1,24 @@
-/* const user = {
-    id: 1,
-    name: 'a h shobuj',
-    address: {
-        village: 'Joyag',
-        city: 'Cumilla',
-        town: 'Dhaka',
-        country: 'BD',
-    },
+const fetchingUsers = () => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then((response) => response.json())
+        .then((users) => displayUsersData(users));
 };
 
-const convertJson = JSON.stringify(user);
-console.log(convertJson);
+function displayUsersData(users) {
+    const parent = document.getElementById('parent');
 
-const convertObj = JSON.parse(convertJson);
-console.log(convertObj);
-
-//* Data Fetching
-fetch('https://jsonplaceholder.typicode.com/users')
-    .then((response) => response.json())
-    .then((users) => console.log(users)); */
-
-const fetchUsers = () => {
-    fetch('https://jsonplaceholder.typicode.com/users/1')
-        .then((res) => res.json())
-        .then((user) => console.log(user));
-};
+    parent.content = '';
+    users.forEach((user) => {
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <div class="bg-gray-300 shadow-xl text-center">
+                        <h2 class="text-5xl">Id: ${user.id} </h2>
+                        <h3 class="text-2xl font-bold text-amber-800">Name: ${user.name}</h3>
+                        <p class="text-xl font-bold">Email: ${user.email}}</p>
+                        <p class="text-xl font-bold">Company: ${user.company.name}}</p>
+                        <p class="text-xl font-bold">City: ${user.address.city}}</p>
+                    </div>
+        `;
+        parent.appendChild(div);
+    });
+}
